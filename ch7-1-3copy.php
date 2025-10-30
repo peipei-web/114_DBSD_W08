cfbf4afa9b9c53355718dc866c192a96b8db8b63
peipei <C112181137@nkust.edu.tw>
2025-10-30 11:48:16 +0800

    message

---
+++ ch7-1-3copy.php
@@ -0,0 +1,17 @@
+<H1><SID:C112181137>
+<br>
+name: Pei pei
+<p>
+
+<?php 
+// 指定陣列元素
+$scores = array(22, 16, 30, 24);
+$total = 0;
+// 使用foreach迴圈計算小費總和
+foreach ($scores as $scores) {
+   echo "$scores | ";
+   $total += $scores;
+}
+
+echo "<br>得分總和: ". $total. "<br>";
+print_r($scores);   // 顯示小費
